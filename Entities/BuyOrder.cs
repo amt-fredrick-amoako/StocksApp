11accd7bc488b6ec82ef0d7eade81ac125f93e13
fredrick amoako <fredrickamoako1@gmail.com>
2023-01-31 00:08:19 +0000

    Added Entity Models

---
+++ Entities/BuyOrder.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class BuyOrder
+    {
+        public Guid BuyOrderID { get; set; }
+
+        [Required]
+        public string StockSymbol { get; set; }
+
+        [Required]
+        public string StockName { get; set; }
+
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [MaxLength(1000)]
+        public int Quantity { get; set; }
+
+        [MaxLength(1000)]
+        public double Price { get; set; }
+
+    }
+}
---
+++ Entities/SellOrder.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class SellOrder
+    {
+        public Guid SellOrderID { get; set; }
+
+        [Required]
+        public string StockSymbol { get; set; }
+
+        [Required]
+        public string StockName { get; set; }
+
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 10000)]
+        public int Quantity { get; set; }
+
+        [Range(1, 10000)]
+        public double Price { get; set; }
+    }
+}
